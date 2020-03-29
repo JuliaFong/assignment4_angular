aaadedd69253b97aa123b3711c28fac8ee9702bc
Julia Fong <Juliamzfong@gmail.com>
2020-03-29 13:33:16 -0500

    Add game-control components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'assignment4';
+  onIntervalFired(firedNumber: number){
+    console.log(firedNumber);
+  }
 }
